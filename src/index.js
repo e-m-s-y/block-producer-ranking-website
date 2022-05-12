b84f6621a73f0169d7382f477748d86f21d4bd33
e-m-s-y <10279407+e-m-s-y@users.noreply.github.com>
2022-05-12 19:40:33 +0200

    release: 1.0.2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const axios = require('axios');
 const path = require('path');
 const app = express();
 
@@ -7,6 +8,15 @@ dotenv.config();
 
 app.use(express.static('./public'));
 app.use('/favicon.ico', express.static('public/img/favicon.png'));
+app.get('/api/delegates/ranking', async (req, res) => {
+    const response = await axios.get(process.env.DELEGATE_RANKING_ENDPOINT);
+
+    if (response.status === 200) {
+        return res.send(response.data);
+    }
+
+    res.status(500).send("Could not get delegate ranking from relay.");
+});
 app.use((req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
